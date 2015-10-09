ac670f297d6b58a13aa5078de5d2380ca120dd08
= <sepand.haghighi@yahoo.com>
2015-10-09 20:59:12 +0330

    docs : Commenting

--- setup.py
+++ setup.py
@@ -1,28 +1,28 @@
-import os
-import shutil
-work_dir= os.getcwd()
+import os 
+import shutil #Library For Work With File In High Level Like Copy
+work_dir= os.getcwd() # Get Current Directory
 image_dir = work_dir+"\\image"
 doc_dir=work_dir+"\\doc"
 out_dir=work_dir+"\\output"
-page_name=["Home","Resume","Project","About Me"]
-actual_name=["index","resume","project","about"]
+page_name=["Home","Resume","Project","About Me"] # list of Page Title In Menu Maker
+actual_name=["index","resume","project","about"] # List Of Pages Actual name like index.html
 break_line="<hr></hr>\n"
-def menu_maker():
+def menu_maker(): # Top Menu Maker In each html page
     result=""
     for i in range(len(page_name)):
         
-        result=result+'\t<a href="'+actual_name[i]+'.html">'+page_name[i]+"</a>\n"
+        result=result+'\t<a href="'+actual_name[i]+'.html">'+page_name[i]+"</a>\n" #  Hyper Link To Each Page In HTML File
         result=result+"&nbsp\n"
-    result=result+"\t\t"+break_line
-    return result
-def menu_writer():
+    result=result+"\t\t"+break_line # Add Break line to End Of The Menu
+    return result # Return All Of The Menu
+def menu_writer():  # Write menu_maker output in html file
     message=menu_maker()
     for i in range(len(page_name)):
         file=open(out_dir+"\\"+actual_name[i]+".html","a")
         file.write(message)
         file.close()
         
-def html_init(name):
+def html_init(name): # Create Initial Form Of each Html Page Like Title And HTML  And Body Tag
     html_name= out_dir+"\\"+name+".html"
     file=open(html_name,"w")
     file.write("<html>\n")
@@ -34,13 +34,13 @@ def html_init(name):
     file.write("\t</head>\n")
     file.write("\t<body>\n")
     file.close()
-def html_end(name):
+def html_end(name):   # Create End Of The Html file
     html_name= out_dir+"\\"+name+".html"
     file=open(html_name,"a")
     file.write("\t</body>\n")
     file.write("</html>")
     file.close()
-def print_text(text_file,file,center=False):
+def print_text(text_file,file,center=False): # Write Text Part Of Each Page
     text_code=""
     header_start="<h4>"
     header_end="</h4>"
@@ -48,9 +48,9 @@ def print_text(text_file,file,center=False):
     for line in text_file:
         line.strip()
         text=line
-        if len(line)==1:
+        if len(line)==1: # For Detecting White Space
             text_code=space
-        else:
+        else:   # Detecting Font Size
             if line.find("[L]")!=-1:
                 header_start="<h2>"
                 header_end="</h2>"
@@ -61,19 +61,19 @@ def print_text(text_file,file,center=False):
                 text=line[3:]
             elif line.find("[M]")!=-1:
                 text=line[3:]
-        if center==True:
+        if center==True: # Centerizes Text If Condition Is True
             header_start="<center>"+header_start
             header_end=header_end+"</center>"
         text_code=header_start+text+header_end+"\n"
         file.write(text_code)
         
     file.close()
-def print_image(file,close=False):
+def print_image(file,close=False): # Write Image Part OF The Page
     image_code='<center><img src="image.jpg" , width=360px></img></center>\n'
     file.write(image_code)
     if close==True:
         file.close()
-def print_download(file,name,link,center=False,close=False):
+def print_download(file,name,link,center=False,close=False): # Create Download Link In Page
     link_code="<a href="+'"'+link+'"'+">"+name+"</a>"
     if center==True:
         link_code="<center>"+link_code+"</center>"
@@ -81,7 +81,7 @@ def print_download(file,name,link,center=False,close=False):
     file.write(break_line)
     if close==True:
         file.close()
-def contain(name):
+def contain(name): # main fucntion That Open Each Page HTML File and call other function to write data in it
         file=open(out_dir+"\\"+name+".html","a")
         text_file=open(doc_dir+"\\"+name+".txt","r")
         resume_name=""
@@ -106,7 +106,7 @@ def contain(name):
             print_text(text_file,file)        
         else:
             print_text(text_file,file)
-def clear_folder(path):
+def clear_folder(path): # This Function Get Path Of Foldr And Delte Its Contains
     list_of_files=os.listdir(path)
     for file in list_of_files:
         print
@@ -125,7 +125,8 @@ if __name__=="__main__":
             print("Homepage is ready")
             print("Upload out and image folder contains directly to your host")
             print("Please Dont Change HTML Files Name")
-        except FileNotFoundError:
+            input("")
+        except FileNotFoundError: # error exception in FileNotFound ( When Something Missed)
             print ("Some File Missed!!")
             print ("Please Check Following :\n 1.Where Is Your Resume File? It should be in doc folder  \n 2.Where is your profile image file? it should be in image folder")
             print (" 3.Where is each page description text file? They Should be in doc folder ")
