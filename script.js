24f9b53cec2812863dbe032f0f447e90737ef270
sepandhaghighi <sepand.haghighi@yahoo.com>
2016-11-07 22:07:46 +0330

    fix : contact menu added

--- script.js
+++ script.js
@@ -4,3 +4,21 @@ function message(){
     alert(a);
     
 }
+
+
+function func1(w){
+    var b=document.getElementsByTagName("body");
+    if (w==1){
+        b[0].style.background="linear-gradient(black,white)";
+    }
+    else if (w==2){
+        b[0].style.background="linear-gradient(red,white)";
+    }
+    else if (w==3){
+        b[0].style.background="linear-gradient(blue,white)";
+    }
+    else{
+        b[0].style.background="linear-gradient(orange,white)";
+    }
+    
+}
