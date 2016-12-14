9c886675a134b6951372f5ee36711bbd09f839e6
sepandhaghighi <sepand.haghighi@yahoo.com>
2016-12-14 14:09:52 +0330

    docs : setup.py

--- setup.py
+++ setup.py
@@ -28,61 +28,61 @@ def file_handler():
     close_files()
 
 def main_handler_2():
-    file_handler()
+    file_handler() # call file_handler
     print("Homepage is ready")
     print("Upload output folder contains directly to your host")
     print("Please Don't Change HTML Files Name")
-    address_print()
-    print_warning()
-    logger(True)
-    if internet():
-        server()
-    browse = int(input("Preview Homepage?[1] or Not[2]"))
-    if browse == 1:
-        preview()
-        close_files()
+    address_print() # print files location
+    print_warning() # print all of the detected warnings
+    logger(True) # add success run of qpage to local logger
+    if internet(): # check internet connection
+        server() # send query to qpage server
+    browse = int(input("Preview Homepage?[1] or Not[2]")) # get input from user for preview of site
+    if browse == 1: # check browse status
+        preview() # call preview function
+        close_files() # close all of the open files
 
 def response_handler(response):
-    if response:
+    if response: # check reposne status
         print(
             "At least one of the folders create for the first time ,\n"
             " please put your data in proper order and run program again\n Program Reboot Automaticly in 3 Sec")
-        wait_func(3)
-        main_handler(False)
-        sys.exit()
+        wait_func(3)# wait for 3 sec
+        main_handler(False) # call main_handler again with False version control flag
+        sys.exit() # exit program
 def sample_handler():
-    response=input("Press [S] to enter sample site material runing or other keys to continue with your data")
-    print_line(70)
-    if response.upper()=="S":
-        sample_site_download(is_sample_downloaded())
+    response=input("Press [S] to enter sample site material runing or other keys to continue with your data") # Get Input form user for loading sample files or continue
+    print_line(70) # print line
+    if response.upper()=="S": # check response status
+        sample_site_download(is_sample_downloaded()) # Call sample download
 def main_handler(control_flag=True):
     try:
-        response = create_folder()
+        response = create_folder() # Check Folder and Files Status
         print("QPAGE By S.Haghighi & M.M.Rahimi")
         print("Version : " + version)
-        address_print()
-        if control_flag==True:
-            version_control()
-        response_handler(response)
-        sample_handler()
+        address_print() # Print Files Location
+        if control_flag==True: # Check if version control passed in prev step
+            version_control() # Check for new version of qpage
+        response_handler(response) # call response_handler
+        sample_handler() # run sample handler
         clear_folder(out_dir)  # clear all of files in output directory
         page_name_update()  # update page names
-        main_handler_2()
+        main_handler_2() # call part_2 of main_handler
     except FileNotFoundError:  # error exception in FileNotFound ( When Something Missed)
-        logger(False)
-        error_handler()
+        logger(False) # Add Failed Run to local logger file
+        error_handler() # call error_handler
     except ValueError:
         print("Bad Input")
-        logger(False)
-        close_files()
-        enter_to_exit()
-        main_handler()
+        logger(False) # Add Failed Run to local logger file
+        close_files() #Close all of the opne files
+        enter_to_exit() # get input from user to continue
+        main_handler() #call part_1 of main_handler , restart from the first
     except PermissionError:
-        logger(False)
+        logger(False) # Add Failed Run to local logger file
         print("Files Is Open By Another Program")
-        close_files()
-        enter_to_exit()
-        main_handler()
+        close_files() #Close all of the opne files
+        enter_to_exit() # get input from user to continue
+        main_handler() #call part_1 of main_handler , resetart from the first
 
 if __name__ == "__main__":
     main_handler()
